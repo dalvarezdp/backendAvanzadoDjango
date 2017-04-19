50947f21a0da7955e9ad48699f303c62bdcde749
David Álvarez <david11988@gmail.com>
2017-04-19 22:33:02 +0200

    Generamos tests de login de usuario y creamos login

--- ui/tests.py
+++ ui/tests.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.urls import reverse
+
 from django.test import TestCase, Client, override_settings
 
 
@@ -6,6 +10,51 @@ class HomePageTest(TestCase):
 
     def test_home_page_for_not_logged_user(self):
         client = Client()
-        response = client.get('/')
+        response = client.get(reverse('ui-home'))
         self.assertEquals(response.status_code, 200)
         self.assertContains(response, "<h1>Welcome to Instageek</h1>")
+
+
+@override_settings(ROOT_URLCONF="ui.urls")
+class ChangeLanguageTests(TestCase):
+
+    def test_change_language(self):
+        client = Client()
+
+        # entramos al home por primera vez
+        response = client.get(reverse('ui-home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<h1>Welcome to Instageek</h1>")
+
+        # cambiamos el idioma a castellano
+        response = client.get(reverse('change-language', kwargs={'language': 'es'}))
+        self.assertEquals(response.status_code, 302)
+
+        # hago una peticion a la url de redireccion
+        response = client.get(response.url)
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<h1>Bienvenido a Instageek</h1>")
+
+        # cambiamos el idioma a ingles
+        response = client.get(reverse('change-language', kwargs={'language': 'en'}))
+        self.assertEquals(response.status_code, 302)
+
+        # hago una peticion a la url de redireccion
+        response = client.get(response.url)
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "<h1>Welcome to Instageek</h1>")
+
+
+@override_settings(ROOT_URLCONF="ui.urls")
+class LoginTest(TestCase):
+
+    def test_login_success(self):
+        User.objects.create_superuser(username='david', email='david@gmail.com', password='supersegura')
+
+        client = Client()
+        response = client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200)
+
+        response = client.post(reverse('login'), {'username': 'david', 'password': 'supersegura'})
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, settings.LOGIN_REDIRECT_URL)
--- ui/urls.py
+++ ui/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView
 
 from ui.views import HomePage, ChangeLanguage
 
 urlpatterns = [
     url(r'^$', HomePage.as_view(), name="ui-home"),
+    url(r'^login$', LoginView.as_view(template_name="ui/login.html"), name="login"),
     url(r'^change-language/(?P<language>.+)$', ChangeLanguage.as_view(), name="change-language")
 ]
