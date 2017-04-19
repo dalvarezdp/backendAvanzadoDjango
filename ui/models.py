5f38b3a2433e63834b0c30c1b38348eac215d2e0
David Álvarez <david11988@gmail.com>
2017-04-19 23:33:28 +0200

    Creo el modelo de post

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+
+    owner = models.ForeignKey(User)
+    image = models.FileField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.image
