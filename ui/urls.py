3e61adef88231a8298afbfbec0aecfbbe0dfd316
David Álvarez <david11988@gmail.com>
2017-04-19 21:44:44 +0200

    Generamos la lista de idiomas y permitimos el idioma del usuario durante toda la sesion

--- ui/urls.py
+++ ui/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from ui.views import HomePage
+from ui.views import HomePage, ChangeLanguage
 
 urlpatterns = [
     url(r'^$', HomePage.as_view(), name="ui-home"),
+    url(r'^change-language/(?P<language>.+)$', ChangeLanguage.as_view(), name="change-language")
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.utils import translation
 
 
 class HomePage(View):
 
     def get(self, request):
         return render(request, 'ui/home.html')
+
+
+class ChangeLanguage(View):
+
+    def get(self, request, language):
+        '''
+        Modificamos el LANGUAGE_SESSION_KEY para mantener el idioma en toda la sesion
+        :param request:
+        :param language:
+        :return:
+        '''
+        request.session[translation.LANGUAGE_SESSION_KEY] = language
+        return redirect(request.META.get("HTTP_REFERER", "ui-home"))
