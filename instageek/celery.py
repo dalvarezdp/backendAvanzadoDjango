44b99dc0837d5994902b6c14617169273597ff01
David Álvarez <david11988@gmail.com>
2017-04-27 19:20:37 +0200

    Creamos una tarea para ejecutar desde un worker de celery

--- instageek/celery.py
+++ instageek/celery.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+from celery import Celery, shared_task
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instageek.settings')
@@ -20,3 +20,7 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+@shared_task
+def helloworld(name):
+    print("Hello from Celery {0}".format(name))
